bd8442cc36cfa69fd87f1d7d9a9c5b2fcf039d3d
Bhagyaraju Pati <bhagyaraju.pati@kore.com>
2022-11-06 22:05:52 +0530

    icons added for app bar in web and mobile

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,67 @@
 import React from 'react'
+import { AppBar, IconButton, Drawer, Button, Avatar, useMediaQuery } from '@mui/material';
+import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
+import { Toolbar } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { useTheme } from '@mui/styles';
 
-const NavBar = () => {
+import useStyles from './styles';
+
+const NavBar = () =>{
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(max-width:600px)');
+    const theme = useTheme();
+    const isAuthenticated = true;
+
   return (
     <div>
+        <>
+            <AppBar position='fixed'>
+                <Toolbar className={classes.toolBar}>
+                    {isMobile && (
+                        <IconButton
+                            color="inherit"
+                            edge="start"
+                            style={{outline: 'none'}}
+                            onClick={()=> {}}
+                            className={classes.menuButton}
+                        >
+                            <Menu />
+                        </IconButton>
+                    )}
+                    <IconButton color="inherit" sx={{ ml: 1 }} onClick={()=> {}}>
+                        {theme.palette.mode === 'dark'? <Brightness7 />: <Brightness4 />}
+                    </IconButton>
 
-        NavBar
+                    {!isMobile && 'Search.......'}
+
+                    <div>
+                        {!isAuthenticated? (
+                            <Button color="inherit" onClick={()=>{}}>
+                                Login &nbsp; <AccountCircle />
+                            </Button>
+                        ):(
+                            <Button 
+                            color="inherit" 
+                            component={ Link }
+                            to={`/profile/123`}
+                            className={classes.linkButton}
+                            onClick={ ()=>{} }
+                            >
+                               {!isMobile && <> My Movies &nbsp; </>}
+                                    <Avatar 
+                                        style={{width:30, height:30}}
+                                        alt="Profile"
+                                        src='https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png'
+                                    />
+                              
+                            </Button>
+                        )}
+                    </div>
+                    {isMobile && 'Search.......'}
+                </Toolbar> 
+            </AppBar>
+        </>
     </div>
   )
 }

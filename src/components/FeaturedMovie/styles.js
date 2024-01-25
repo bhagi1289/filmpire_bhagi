e7dd3abf80ac6e49b0b58c8f5463b9d099174344
Bhagyaraju <bhagyaraju1289@gmail.com>
2024-01-25 17:50:25 +0530

    css aligned

--- src/components/FeaturedMovie/styles.js
+++ src/components/FeaturedMovie/styles.js
@@ -2,27 +2,29 @@ import { makeStyles } from '@mui/styles';
 
 export default makeStyles((theme) => ({
   featuredCardContainer: {
-    marginBottom: '160px',
+    marginBottom: '50px',
     display: 'flex',
     justifyContent: 'center',
-    height: '490px',
+    height: '750px',
     textDecoration: 'none',
   },
   card: {
-    width: '100%',
+    // width: '100%',
     display: 'flex',
     justifyContent: 'flex-end',
     flexDirection: 'column',
   },
   cardRoot: {
-    postion: 'relative',
+    position: 'relative',
   },
   cardMedia: {
     position: 'absolute',
-    top: '95px',
-    right: 0,
-    height: '593px',
-    width: '1275px',
+    top: '0', // Changed from '95px' to '0' to include the top part of the image
+    right: '0',
+    bottom: '0', 
+    objectFit: 'cover',
+    height: '100%',
+    width: '100%',
     backgroundColor: 'rgba(0,0,0,0.575)',
     backgroundBlendMode: 'darken',
   },
